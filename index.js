177634034971d1f41b3984a6522ff7753942818d
Mike Chaliy <mike@chaliy.name>
2016-03-02 18:48:40 +0200

    Added hello world

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
